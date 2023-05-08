909650351b27d33ff19717c62b736a5b10ba907f
Noah Hoang <hoincbusiness@gmail.com>
2023-05-07 21:08:03 -0700

    Added feature to import necessary modules using dotenv package and sequelize

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    });
+
+export default sequelize;
